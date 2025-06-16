4c8b54430f0647bf404b34c3795ea4893a421515
tmathis720 <tmathis720@gmail.com>
2025-06-15 23:21:31 -0700

    Refactor preconditioner enums by moving PC and SparsityPattern definitions to pc_context.rs for better organization

--- src/preconditioner/mod.rs
+++ src/preconditioner/mod.rs
@@ -39,22 +39,4 @@ pub use approxinv::ApproxInv;
 pub use self::sor::MatSorType;
 
 /// Unified preconditioner enum for all supported types.
-pub enum PC<T> {
-    Jacobi,
-    Ssor,
-    Ilu0,
-    Ilup { fill: usize },
-    Ilut { fill: usize, droptol: T },
-    Chebyshev { degree: usize, emin: Option<T>, emax: Option<T> },
-    ApproxInv { pattern: SparsityPattern, tol: T, max_iter: usize },
-    BlockJacobi { blocks: Vec<Vec<usize>> },
-    Multicolor { colors: Vec<usize> },
-    AMG,
-    AdditiveSchwarz,
-}
-
-/// Sparsity pattern for approximate inverse preconditioners.
-pub enum SparsityPattern {
-    Auto,
-    Manual(Vec<Vec<usize>>), // for each row, the list of column indices
-}
+pub use crate::context::pc_context::{PC, SparsityPattern};
