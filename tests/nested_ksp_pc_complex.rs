26e4e5372b9296ed4274a03edf416203b0d3e9f1
Thomas Mathis <34523436+tmathis720@users.noreply.github.com>
2026-03-27 16:12:55 -0700

    Fix complex nested KSP test trait method resolution

--- tests/nested_ksp_pc_complex.rs
+++ tests/nested_ksp_pc_complex.rs
@@ -4,6 +4,7 @@ use std::sync::Arc;
 
 use faer::Mat;
 use kryst::algebra::prelude::S;
+use kryst::algebra::KrystScalar;
 use kryst::config::options::{KspOptions, PcOptions};
 use kryst::context::ksp_context::{KspContext, SolverType};
 use kryst::matrix::op::DenseOp;
